bddc046d6c357b5c83f900798818781f241fcdaf
Estudiantes <Estudiantes@Labing50410.udistrital.edu.co>
2018-02-09 11:46:06 -0500

    calculadora primer clase

---
+++ JavaApplication3/src/javaapplication3/Calculadora.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calculadora {
+ 
+
+    int x;
+    int y;
+    int r;
+
+    void sumar() {
+        r = x + y;
+    }
+    void restar(){
+        if (x<y){
+            int a;
+            a=x;
+            x=y;
+            y=a;
+            
+        }
+        if(x<0){
+            x=-x;
+            y=-y;
+                    
+        }
+        r=x-y;
+       
+    }
+}
+
+}
