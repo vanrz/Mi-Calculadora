083d826d265b0514b5b764f92e5086331eaf0de2
Estudiantes <Estudiantes@Labing70204>
2018-02-12 17:16:36 -0500

    4444

--- JavaApplication3/src/micalculadora/Calculadora.java
+++ JavaApplication3/src/micalculadora/Calculadora.java
@@ -1,10 +1,20 @@
-package micalculadora;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculadora;
 
+/**
+ *
+ * @author Estudiantes
+ */
 public class Calculadora {
+ 
 
-    int x;
-    int y;
-    int r;
+    private int x;
+    private int y;
+    private int r;
 
     void sumar() {
         r = x + y;
@@ -25,4 +35,19 @@ public class Calculadora {
         r=x-y;
        
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getR() {
+        return r;
+    }
+    
 }
+
+
